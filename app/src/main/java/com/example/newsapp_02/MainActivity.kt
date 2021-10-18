7d2875ba89af49a02a21eacc754e21b4c02ece04
beslimir <marin.beslo@gmail.com>
2021-10-18 17:30:33 +0200

    remove initial comment

--- app/src/main/java/com/example/newsapp_02/MainActivity.kt
+++ app/src/main/java/com/example/newsapp_02/MainActivity.kt
@@ -3,16 +3,6 @@ package com.example.newsapp_02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-/**
- *
- * Use cases:
- * - Get news headlines
- * - Save news
- * - Get saved news
- * - Delete saved news
- * - Search news
- * **/
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
